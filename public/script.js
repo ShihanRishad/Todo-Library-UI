d88158765d7d0c1d816f1349076b491a8b325d15
Shihan Rishad <165176498+ShihanRishad@users.noreply.github.com>
2024-10-15 15:23:19 +0000

    ux improvements changed some ui design, added font.

--- public/script.js
+++ public/script.js
@@ -1,46 +1,48 @@
-const app = new TodoApp();
-function dom(query) { // Just do it faster 😂
+const app = new TodoApp(); // Get the app's class from the library
+function dom(query) { // Just do it faster 😁
   return document.querySelector(query);
 }
 var listscroll = dom(".listscroll");
+var addbtn = dom("#addbtn");
+
+addbtn.addEventListener('click', function(event) {
+  event.preventDefault();
+});
+
 let listheight;
 setTimeout(() => {
   listscroll.scrollTo({
     top: listscroll.scrollHeight,
     behavior: 'smooth'
-  });
-  listheight = listscroll.scrollHeight;
-
+  }); // Scroll to bottom after the page loads
 }, 500);
   
-function renderTasks() {
+function renderTasks() { // Function to show all the task
   const taskList = document.getElementById('taskList');
   taskList.innerHTML = '';
-  app.getTasks().forEach((task, index) => {
-    const taskItem = document.createElement('md-list-item');
-    taskItem.className = 'task' + (task.completed ? ' completed' : '');
-    // let tasktype = taskItem.createAttribute("type");
-    // tasktype.value = "button";
-    taskItem.type = "button";
+  app.getTasks().forEach((task, index) => { // First get the tasks, then take task and their index as an argument
+    const taskItem = document.createElement('md-list-item'); // Create one for every element
+    taskItem.className = 'task' + (task.completed ? ' completed' : ''); // Add a class name to check if it's completed or not
+    taskItem.type = "button"; // For material design
     taskItem.innerHTML = `
       <span slot="headline">${task.task}</span>
       <span slot="end">
-            <md-icon-button onclick="markAsCompleted(${index})">
+            <md-icon-button onclick="markAsCompleted(${index})"> 
               <md-icon><i class="material-symbols-rounded">check</i></md-icon>
             </md-icon-button>
             <md-icon-button onclick="removeTask(${index})">
               <md-icon><i class="material-symbols-rounded">delete</i></md-icon>
             </md-icon-button>
       </span>
-    `;
+    `; // There are some material design elements, for better mobile experience.
     taskList.appendChild(taskItem);
   });
 }
 
 function addTask() {
   const newTask = document.getElementById('newTask').value;
-  if (newTask) {
-    app.addTask(newTask);
+  if (newTask) { // Do it if the input is not empty
+    app.addTask(newTask); // Add task with the function from the libray
     document.getElementById('newTask').value = '';
     renderTasks();
   }
@@ -50,21 +52,18 @@ setTimeout(() => {
       top: listscroll.scrollHeight,
       behavior: 'smooth'
     });
-}, 100);
+}, 100); // Take a deley because it might take some time to update dom
 }
 
 document.querySelector("#newTask").addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter') { // Handle keyboard event
       addTask()
     }
   });
 
 function removeTask(index) {
-  let sure = true
-  if (sure) {
-    app.removeTask(index);
-  }
-  renderTasks();
+    app.removeTask(index); // Remove function from library function
+    renderTasks();
 }
 
 function markAsCompleted(index) {
